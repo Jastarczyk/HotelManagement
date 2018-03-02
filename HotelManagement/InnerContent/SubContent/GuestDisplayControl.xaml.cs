f427d90f2175aeac4840f30434294012a1b7e02b
Selectus <selectuss@gmail.com>
2018-03-02 21:07:39 +0100

    Starting work on guest display control (Push to work transfer on another computer)

---
+++ HotelManagement/InnerContent/SubContent/GuestDisplayControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManagement.InnerContent.SubContent
+{
+    /// <summary>
+    /// Interaction logic for GuestDisplayControl.xaml
+    /// </summary>
+    public partial class GuestDisplayControl : UserControl
+    {
+        public GuestDisplayControl()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TODO make it done!
+            //this.GuestDataGrid.ItemsSource = 
+        }
+    }
+}
